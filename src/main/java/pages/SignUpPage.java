159b784bd4ef74db77f61298d643c09bd649863e
xkodxdf <sirota92@bk.ru>
2023-01-25 15:09:59 +0300

    SignUpPage added

--- src/main/java/pages/SignUpPage.java
+++ src/main/java/pages/SignUpPage.java
@@ -1,2 +1,11 @@
-package pages;public class SignUpPage {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import pages.base_abstract.NavigationBarPage;
+
+public class SignUpPage extends NavigationBarPage {
+
+    protected SignUpPage(WebDriver driver) {
+        super(driver);
+    }
 }

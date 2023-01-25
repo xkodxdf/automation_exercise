7bbc086011d2d9be40badf6999c5affb75b78e02
xkodxdf <sirota92@bk.ru>
2023-01-25 15:05:41 +0300

    clickSignUpLogin() method and sign up/login locator added

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,12 +1,30 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
-import pages.base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.base_abstract.BasePage;
 
 
 public class MainPage extends BasePage {
 
+    @FindBy(xpath = "//a[@href='/login']")
+    private WebElement signUpLoginLink;
+
+    public WebElement getSignUpLoginLink() {
+
+        return signUpLoginLink;
+    }
+
+
     public MainPage(WebDriver driver) {
         super(driver);
+    }
+
+
+    public LoginPage clickSignUpLogin() {
+        click(signUpLoginLink);
+
+        return new LoginPage(getDriver());
     }
 }

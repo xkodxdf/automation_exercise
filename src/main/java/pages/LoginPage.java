b8e1380d974cd6f509e297d8ece093f71e6c551b
xkodxdf <sirota92@bk.ru>
2023-01-25 15:08:35 +0300

    LoginPage added

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,2 +1,14 @@
-package pages;public class LoginPage {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import pages.base_abstract.BasePage;
+import pages.base_abstract.NavigationBarPage;
+
+import java.util.jar.Attributes;
+
+public class LoginPage extends NavigationBarPage {
+
+    protected LoginPage(WebDriver driver) {
+        super(driver);
+    }
 }

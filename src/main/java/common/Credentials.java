6addfe3f71e5e0c17b212e54100903750748b650
xkodxdf <sirota92@bk.ru>
2023-01-28 22:46:19 +0300

    added Credentials class with creds for sign up

---
+++ src/main/java/common/Credentials.java
@@ -0,0 +1,95 @@
+package common;
+
+public class Credentials {
+
+    private final String name = "test";
+    private final String email = "onemoretest@fortest.com";
+    private final String password = "letmeinplease";
+    private final String firstName = "Ed";
+    private final String lastName = "Edwards";
+    private final String dayOfBirth = "11";
+    private final String monthOfBirth = "11";
+    private final String yearOfBirth = "1984";
+    private final String address = "19 Test st.";
+    private final String address2 = "22 Selenium st.";
+    private final String country = "India";
+    private final String state = "test state";
+    private final String city = "New Test";
+    private final String zipcode = "4434523";
+    private final String mobileNumber = "331210";
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public String getLastName() {
+
+        return lastName;
+    }
+
+    public String getDayOfBirth() {
+
+        return dayOfBirth;
+    }
+
+    public String getMonthOfBirth() {
+
+        return monthOfBirth;
+    }
+
+    public String getYearOfBirth() {
+
+        return yearOfBirth;
+    }
+
+    public String getAddress() {
+
+        return address;
+    }
+
+    public String getAddress2() {
+
+        return address2;
+    }
+
+    public String getCountry() {
+
+        return country;
+    }
+
+    public String getState() {
+
+        return state;
+    }
+
+    public String getCity() {
+
+        return city;
+    }
+
+    public String getZipcode() {
+
+        return zipcode;
+    }
+
+    public String getMobileNumber() {
+
+        return mobileNumber;
+    }
+}

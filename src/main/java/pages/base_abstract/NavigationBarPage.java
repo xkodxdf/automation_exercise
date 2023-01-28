5c6e0ebad18188352c1cf4fea12876af3e568ce9
xkodxdf <sirota92@bk.ru>
2023-01-28 22:52:06 +0300

    constructor access modifier changed

--- src/main/java/pages/base_abstract/NavigationBarPage.java
+++ src/main/java/pages/base_abstract/NavigationBarPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationBarPage extends BasePage {
 
-    protected NavigationBarPage(WebDriver driver) {
+    public NavigationBarPage(WebDriver driver) {
         super(driver);
     }
 }

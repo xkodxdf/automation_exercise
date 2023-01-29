a0c2307a3cc16a27b356810c3cd55e43e187713e
xkodxdf <sirota92@bk.ru>
2023-01-29 13:37:42 +0300

    locators, getters, methods added

--- src/main/java/pages/DeleteAccountPage.java
+++ src/main/java/pages/DeleteAccountPage.java
@@ -1,11 +1,43 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.base_abstract.NavigationBarPage;
 
 public class DeleteAccountPage extends NavigationBarPage {
 
-    protected DeleteAccountPage(WebDriver driver) {
+    @FindBy(xpath = "//h2[@data-qa='account-deleted']/b")
+    private WebElement accountDeletedText;
+
+    @FindBy(xpath = "//a[@data-qa='continue-button']")
+    private WebElement continueButton;
+
+
+    public DeleteAccountPage(WebDriver driver) {
         super(driver);
     }
+
+
+    public WebElement getAccountDeletedText() {
+
+        return accountDeletedText;
+    }
+
+    public WebElement getContinueButton() {
+        return continueButton;
+    }
+
+
+    public boolean isAccountDeletedTextDisplayed() {
+
+        return verifyElementVisibility(accountDeletedText);
+    }
+
+    public MainPage clickContinueButton() {
+        click(continueButton);
+
+        return new MainPage(getDriver());
+    }
+
 }

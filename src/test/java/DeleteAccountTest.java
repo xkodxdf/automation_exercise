a93baf84adde99aec910b5a17de2081d0d886305
xkodxdf <sirota92@bk.ru>
2023-01-29 13:32:42 +0300

    testRegisterUser() added

--- src/test/java/DeleteAccountTest.java
+++ src/test/java/DeleteAccountTest.java
@@ -1,13 +1,45 @@
 import common.BaseTest;
-import org.testng.Assert;
+
+import static org.testng.Assert.assertTrue;
+
+import common.Credentials;
 import org.testng.annotations.Test;
 import pages.MainPage;
+import pages.DeleteAccountPage;
 
-public class MainTest extends BaseTest {
+
+public class DeleteAccountTest extends BaseTest {
 
     @Test
-    public void testRegisterUser() {
+    public void testRegisterUser() throws InterruptedException {
+        Credentials creds = new Credentials();
         MainPage mainPage = openMainPage();
 
+        DeleteAccountPage deleteAccountPage =
+                mainPage.clickSignUpLogin()
+                        .inputSignUpName(creds.getName())
+                        .inputSignUpEmail(creds.getEmail())
+                        .clickSignUpButton()
+                        .chooseMaleGender()
+                        .inputPassword(creds.getPassword())
+                        .inputDateOfBirth(creds.getDayOfBirth(),
+                                creds.getMonthOfBirth(),
+                                creds.getYearOfBirth())
+                        .selectNewsletterCheckBox()
+                        .selectSpecialOffersCheckBox()
+                        .inputFirstName(creds.getFirstName())
+                        .inputLastName(creds.getLastName())
+                        .inputAddress(creds.getAddress())
+                        .inputAddress2(creds.getAddress2())
+                        .selectCountry(creds.getCountry())
+                        .inputState(creds.getState())
+                        .inputCity(creds.getCity())
+                        .inputZipcode(creds.getZipcode())
+                        .inputMobileNumber(creds.getMobileNumber())
+                        .clickCreateAccount()
+                        .clickContinueButton()
+                        .clickDeleteAccount();
+
+        assertTrue(deleteAccountPage.isAccountDeletedTextDisplayed());
     }
 }

ba033c6eee39596e3b7f44011eb0bfec2ec06f1f
xkodxdf <sirota92@bk.ru>
2023-01-29 13:36:30 +0300

    locators, getters, methods added

--- src/main/java/pages/AccountCreatedPage.java
+++ src/main/java/pages/AccountCreatedPage.java
@@ -1,11 +1,43 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.base_abstract.NavigationBarPage;
 
 public class AccountCreatedPage extends NavigationBarPage {
 
-    protected AccountCreatedPage(WebDriver driver) {
+    @FindBy(xpath = "//h2[@data-qa='account-created']/b")
+    private WebElement accountCreatedText;
+
+    @FindBy(xpath = "//a[@data-qa='continue-button']")
+    private WebElement continueButton;
+
+
+    public AccountCreatedPage(WebDriver driver) {
         super(driver);
+    }
+
+
+    public WebElement getAccountCreatedText() {
+
+        return accountCreatedText;
+    }
+
+    public WebElement getContinueButton() {
+
+        return continueButton;
+    }
+
+
+    public boolean isAccountCreatedTextDisplayed() {
+
+        return verifyElementVisibility(accountCreatedText);
+    }
+
+    public MainPage clickContinueButton() {
+        click(continueButton);
+
+        return new MainPage(getDriver());
     }
 }

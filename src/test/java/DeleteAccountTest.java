8a3b68a8a0b1f6db79fe464944877bcf1030638b
xkodxdf <sirota92@bk.ru>
2023-02-07 20:10:13 +0300

    testRegisterUser() refactored

--- src/test/java/DeleteAccountTest.java
+++ src/test/java/DeleteAccountTest.java
@@ -1,40 +1,23 @@
 import common.BaseTest;
-import common.Credentials;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.reporters.jq.Main;
-import pages.AccountCreatedPage;
 import pages.DeleteAccountPage;
-import pages.MainPage;
 
 
 public class DeleteAccountTest extends BaseTest {
 
     @Test
     public void testRegisterUser() {
+        String expectedAccDeletedText = "ACCOUNT DELETED!";
 
-        AccountCreatedPage accountCreatedPage = openMainPage()
+        DeleteAccountPage deleteAccountPage = openMainPage()
                 .clickSignUpLogin()
                 .fillLoginPageSignUpForm()
                 .clickSignUpButton()
                 .fillSignUpPageSignUpForm()
-                .clickCreateAccount();
-
-        String expectedHeaderText = "ACCOUNT CREATED!";
-
-        Assert.assertTrue(accountCreatedPage.isAccountCreatedHeaderDisplayed());
-        Assert.assertEquals(accountCreatedPage.getAccCreatedHeaderText(), expectedHeaderText);
-
-        MainPage mainPage = accountCreatedPage.clickContinueButton();
-
-        String expectedLoggedInText = "Logged in as " + new Credentials().getName();
-
-        Assert.assertTrue(mainPage.isLoggedInAsDisplayed());
-        Assert.assertEquals(mainPage.getLoggedInAsText(), expectedLoggedInText);
-
-        DeleteAccountPage deleteAccountPage = mainPage.clickDeleteAccount();
-
-        String expectedAccDeletedText = "ACCOUNT DELETED!";
+                .clickCreateAccount()
+                .clickContinueButton()
+                .clickDeleteAccount();
 
         Assert.assertTrue(deleteAccountPage.isAccountDeletedTextDisplayed());
         Assert.assertEquals(deleteAccountPage.getAccDeletedHeaderText(), expectedAccDeletedText);

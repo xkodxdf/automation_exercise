6fc8daaef6fb4fff1911ad888dc0eb673d0411e2
Yan Dadaelov <sirota92@bk.ru>
2023-02-20 20:55:08 +0300

    Test case 7 (#15)
    * TestCasesPage added
    
    * testNavigateToTestCasesPage() added

---
+++ src/main/java/pages/TestCasesPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import pages.base_abstract.TopMenuPage;
+
+public class TestCasesPage extends TopMenuPage {
+
+    public TestCasesPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/TestCasesTest.java
@@ -0,0 +1,18 @@
+import common.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestCasesPage;
+
+public class TestCasesTest extends BaseTest {
+
+    @Test
+    public void testNavigateToTestCasesPage() {
+        String expectedTitle = "Automation Practice Website for UI Testing - Test Cases";
+        String expectedUrl = "https://www.automationexercise.com/test_cases";
+
+        TestCasesPage testCasesPage = openMainPage().clickTestCases();
+
+        Assert.assertEquals(testCasesPage.getTitle(), expectedTitle);
+        Assert.assertEquals(testCasesPage.getCurrentUrl(), expectedUrl);
+    }
+}

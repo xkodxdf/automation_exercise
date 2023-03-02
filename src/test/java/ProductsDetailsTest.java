400ea745fe70f0e8930f88de7db4c568d709fd01
xkodxdf <sirota92@bk.ru>
2023-03-02 12:59:41 +0300

    url check added to testViewProduct

--- src/test/java/ProductsDetailsTest.java
+++ src/test/java/ProductsDetailsTest.java
@@ -8,9 +8,13 @@ public class ProductsDetailsTest extends BaseTest {
 
     @Test
     public void testViewProduct() {
+        String productsDetalsUrl = "https://www.automationexercise.com/product_details/";
+
         ProductsDetailsPage productsDetailsPage = openMainPage()
                 .clickProducts()
                 .clickProductListViewItem(1);
+
+        Assert.assertTrue(productsDetailsPage.getCurrentUrl().contains(productsDetalsUrl));
 
         for (WebElement el : productsDetailsPage.getProductInfo()) {
             Assert.assertTrue(productsDetailsPage.verifyElementVisibility(el));

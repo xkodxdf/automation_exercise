0255edaab057f4fb8adf495c83c7b6cb9ff24beb
xkodxdf <sirota92@bk.ru>
2023-03-02 14:05:08 +0300

    testSearchProduct() added

--- src/test/java/ProductsTest.java
+++ src/test/java/ProductsTest.java
@@ -1,6 +1,7 @@
 import common.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.ProductsPage;
 
 public class ProductsTest extends BaseTest {
@@ -15,5 +16,21 @@ public class ProductsTest extends BaseTest {
         Assert.assertEquals(productsPage.getCurrentUrl(), expectedUrl);
         Assert.assertEquals(productsPage.getTitle(), expectedTitle);
         Assert.assertTrue(productsPage.getProductList().isDisplayed());
+    }
+
+    @Test
+    public void testSearchProduct() {
+        String textToSearch = "jeans";
+        String expectedHeaderText = "SEARCHED PRODUCTS";
+        SoftAssert softAssert = new SoftAssert();
+
+        ProductsPage productsPage = openMainPage()
+                .clickProducts()
+                .searchProduct(textToSearch);
+
+        softAssert.assertTrue(productsPage.isH2SearchedProductsDisplayed());
+        softAssert.assertEquals(productsPage.getH2SearchedProducts().getText(), expectedHeaderText);
+        softAssert.assertTrue(productsPage.isSearchedProductsDisplayed());
+        softAssert.assertAll();
     }
 }

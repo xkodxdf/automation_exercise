993ee1a6283851dca9bed88a397c9fa9bdd452e2
Yan Dadaelov <sirota92@bk.ru>
2023-03-27 20:34:26 +0300

    Test case 10 (#17)
    * WebDriverManager added to InitDriver()
    
    * WebDriverManager dependency added
    
    * scrollToElement() added
    
    * FooterPage added
    
    * testSubscription() added
    
    * webdrivers exe files was deleted

---
+++ src/test/java/FooterTest.java
@@ -0,0 +1,24 @@
+import common.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.base_abstract.FooterPage;
+
+public class FooterTest extends BaseTest {
+
+    @Test
+    public void testSubscription() {
+        String expectedH2Text = "SUBSCRIPTION";
+        String expectedSuccessMessage = "You have been successfully subscribed!";
+
+        openMainPage();
+        FooterPage footerPage = new FooterPage(getDriver());
+        footerPage
+                .scrollToSubscription()
+                .inputEmail()
+                .clickSubmitButton();
+
+        Assert.assertEquals(footerPage.getSubscriptionH2text().getText(), expectedH2Text);
+        Assert.assertTrue(footerPage.getSubscriptionSuccessMessage().isDisplayed());
+        Assert.assertEquals(footerPage.getSubscriptionSuccessMessage().getText(), expectedSuccessMessage);
+    }
+}

12c5a8d5f9b7aa9ff72937b1c8a1d9f8cb3faf5a
xkodxdf <sirota92@bk.ru>
2023-04-19 21:50:42 +0300

    CartPage added

---
+++ src/main/java/pages/CartPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import pages.base_abstract.TopMenuPage;
+
+public class CartPage extends TopMenuPage {
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+}

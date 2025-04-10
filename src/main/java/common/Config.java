bd77a8987be2f37f552e4270e32f9664bcbcff5d
xkodxdf <sirotapi@bk.ru>
2025-04-10 22:20:54 +0300

    rem[unusable constants]

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -6,16 +6,13 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 abstract class Config {
 
     final String chromeDriver = "chrome";
-    final String chromeDriverPath = "src/main/resources/web_driver/chromedriver.exe";
     final ChromeOptions chromeOptions = new ChromeOptions();
     final String firefoxDriver = "firefox";
-    final String getGeckoDriverPath = "src/main/resources/web_driver/geckodriver.exe";
     final FirefoxOptions firefoxOptions = new FirefoxOptions();
-    final String BROWSER = chromeDriver;
+    final String BROWSER = firefoxDriver;
     final String CHROME_WINDOW_SIZE = "--window-size=1920,1080";
     final String FIREFOX_WINDOW_WIDTH = "--width=1920";
     final String FIREFOX_WINDOW_HEIGHT = "--height=1080";
     final int IMPLICITLY_WAIT_SEC = 10;
     final int PAGE_LOAD_TIMEOUT_SEC = 180;
-
 }
